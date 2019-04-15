a45e67dbee4574a70051aca01ef4374931449c34
pascalboehler <pascal.boehler@hotmail.de>
2019-04-15 11:15:59 +0200

    - Created ViewController Swift Classes

---
+++ WSIE/RecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeViewController.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 15.04.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
