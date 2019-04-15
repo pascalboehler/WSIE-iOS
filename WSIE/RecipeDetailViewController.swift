9590b53200564a7a9531dd437c37b0baca517a9f
pascalboehler <pascal.boehler@hotmail.de>
2019-04-15 18:16:47 +0200

    - Created RecipeDetailViewController - Embed RecipeViewController inside NavigationViewController - Added "Add" Button (at the moment called "Item")

---
+++ WSIE/RecipeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailViewController.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 15.04.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ WSIE/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 15.04.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
