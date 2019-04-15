5219f9550b7c34c1f5aa170abc5ad056114955f5
pascalboehler <pascal.boehler@hotmail.de>
2019-04-15 20:42:40 +0200

    Added Outlets for RecipeTableViewCell and fixed add icon

---
+++ WSIE/CreateRecipeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateRecipeViewController.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 15.04.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import UIKit
+
+class CreateRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
