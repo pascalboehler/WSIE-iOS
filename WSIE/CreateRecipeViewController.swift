85a2d834b9bb93bb57dcb599e06af7c37c0e8bc9
pascalboehler <pascal.boehler@hotmail.de>
2019-04-18 18:33:41 +0200

    - Changed CreateRecipeViewController GUI to EditRecipeViewController GUI Style...

--- WSIE/CreateRecipeViewController.swift
+++ WSIE/CreateRecipeViewController.swift
@@ -11,19 +11,81 @@ import CoreData
 
 class CreateRecipeViewController: UIViewController {
     
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var shortDescriptionTextView: UITextView!
-    @IBOutlet weak var imageLinkTextView: UITextField!
+    var titleLabel: UILabel!
+    var titleTextField: UITextField!
+    var shortDescriptionLabel: UILabel!
+    var shortDescriptionTextView: UITextView!
+    var pictureLabel: UILabel!
+    var picturePicker: UIButton!
+    var materialsLabel: UILabel!
+    //var materialsTableView: UITableView!
+    var materialsTextView: UITextView!
+    var stepsLabel: UILabel!
+    //var stepsTableView: UITableView!
+    var stepsTextView: UITextView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var currentIndex: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        shortDescriptionTextView.delegate = self
+        
+        self.scrollView.contentSize = CGSize(width: self.view.bounds.width, height: 1000)
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.isScrollEnabled = true
+        
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.scrollView.bounds.width, height: 50))
+        titleLabel.text = "Recipe title: "
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        scrollView.addSubview(titleLabel)
+        
+        titleTextField = UITextField(frame: CGRect(x: 0, y: 58, width: self.scrollView.bounds.width, height: 30))
+        titleTextField.placeholder = "Insert a title for the recipe"
+        self.scrollView.addSubview(titleTextField)
+        
+        pictureLabel = UILabel(frame: CGRect(x: 0, y: 91, width: self.scrollView.bounds.width, height: 50))
+        pictureLabel.text = "Recipe image: "
+        pictureLabel.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        pictureLabel.textAlignment = .center
+        scrollView.addSubview(pictureLabel)
+        
+        picturePicker = UIButton(frame: CGRect(x: 0, y: 154, width: self.scrollView.bounds.width, height: CGFloat(self.scrollView.bounds.width*(2.0/3.0))))
+        picturePicker.backgroundColor = UIColor.lightGray
+        picturePicker.addTarget(self, action: #selector(picturePickerButtonHandler(sender:)), for: .touchUpInside)
+        scrollView.addSubview(picturePicker)
+        
+        materialsLabel = UILabel(frame: CGRect(x: 0, y: Int(154 + Int(self.picturePicker.bounds.height)), width: Int(self.scrollView.bounds.width), height: 50))
+        materialsLabel.text = "Materials: "
+        materialsLabel.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        materialsLabel.textAlignment = .center
+        scrollView.addSubview(materialsLabel)
+        
+        materialsTextView = UITextView(frame: CGRect(x: 0, y: materialsLabel.frame.maxY + 8, width: self.scrollView.bounds.width, height: 150))
+        materialsTextView.isEditable = true
+        materialsTextView.autocapitalizationType = .sentences
+        materialsTextView.autocorrectionType = .default
+        scrollView.addSubview(materialsTextView)
+        
+        stepsLabel = UILabel(frame: CGRect(x: 0, y: materialsTextView.frame.maxY + 8, width: self.scrollView.bounds.width, height: 50))
+        stepsLabel.text = "Steps: "
+        stepsLabel.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        stepsLabel.textAlignment = .center
+        scrollView.addSubview(stepsLabel)
+        
+        stepsTextView = UITextView(frame: CGRect(x: 0, y: stepsLabel.frame.maxY + 8, width: self.scrollView.bounds.width, height: 150))
+        stepsTextView.isEditable = true
+        stepsTextView.autocapitalizationType = .sentences
+        stepsTextView.autocorrectionType = .default
+        scrollView.addSubview(stepsTextView)
+        
         // Do any additional setup after loading the view.
     }
     
-    
+    @objc func picturePickerButtonHandler(sender: UIButton) {
+        print("picturePicker pressed...")
+    }
     
     @IBAction func cancelButtonHandler(_ sender: Any) {
         print("On Cancel Button Pressed")
@@ -33,6 +95,7 @@ class CreateRecipeViewController: UIViewController {
     
     @IBAction func saveButtonHandler(_ sender: Any) {
         print("OnSaveButtonPressed")
+        /*
         guard let title = self.titleTextField.text else {
             self.missingElementAlert(missingElement: "title")
             return
@@ -45,7 +108,7 @@ class CreateRecipeViewController: UIViewController {
             self.missingElementAlert(missingElement: "imageLink")
             return
         }
-        saveRecipe(title: title, shortDescription: shortDescription, imageLink: imageLink)
+        saveRecipe(title: title, shortDescription: shortDescription, imageLink: imageLink) */
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -80,6 +143,7 @@ class CreateRecipeViewController: UIViewController {
         })
     	let saveAlert = UIAlertAction(title: "Save", style: .default, handler: {
             _ in
+            /*
             guard let titleString = self.titleTextField.text else {
                 self.missingElementAlert(missingElement: "title")
                 return
@@ -93,6 +157,7 @@ class CreateRecipeViewController: UIViewController {
                 return
             }
             self.saveRecipe(title: titleString, shortDescription: shortDescriptionString, imageLink: imageLink)
+ */
             self.dismiss(animated: true, completion: nil)
         })
     	alertController.addAction(cancelAlert)
@@ -123,6 +188,7 @@ class CreateRecipeViewController: UIViewController {
     
 }
 
+/*
 extension CreateRecipeViewController : UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if text == "\n" {
@@ -132,3 +198,4 @@ extension CreateRecipeViewController : UITextViewDelegate {
         return true
     }
 }
+*/
