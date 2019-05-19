0cf2297b3c379581224043ac782acc20d51155c5
pascalboehler <pascal.boehler@hotmail.de>
2019-05-19 19:36:23 +0200

    Added 'CheckBox' pod to proj

---
+++ WSIE/ShoppingListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListItemTableViewCell.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 19.05.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
