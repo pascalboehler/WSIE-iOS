37a7244b16eefb4954ecfcdc0d2346850373a3a1
pascalboehler <pascal.boehler@hotmail.de>
2019-05-20 17:24:29 +0200

    Added CoreData Model ShoppingList to ShoppingListViewController + UI work

--- WSIE/ShoppingListViewController.swift
+++ WSIE/ShoppingListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 enum BarButtonStatus {
     case add, done
@@ -21,6 +22,7 @@ class ShoppingListViewController: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
     var barButtonStatus: BarButtonStatus = .add
     
+    var shoppingList: [ShoppingList] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +33,15 @@ class ShoppingListViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        // setup nav bar button
         let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemButtonHandler))
         navigationBar.topItem?.rightBarButtonItem = button
         barButtonStatus = .add
+        
+        // Load data from db
+        shoppingList = fetchData()
+        // reload the tableView's data
+        tableView.reloadData()
     }
 
     @objc func addItemButtonHandler() {
@@ -74,20 +82,40 @@ class ShoppingListViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func addNewItemButtonHandler(_ sender: Any) {
+        print("On addNewItemButton pressed")
+    }
+    
+    // get the data from db
+    func fetchData() -> [ShoppingList]{
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<ShoppingList>(entityName: "ShoppingList")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            return result
+        } catch let error as NSError {
+            // something went wrong, print the error.
+            print(error.description)
+        }
+        return []
+    }
 }
 
 extension ShoppingListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return shoppingList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingListItemTableViewCell") as! ShoppingListItemTableViewCell
-        cell.itemName.text = "Test"
+        // create data
+        cell.itemName.text = "\(shoppingList[indexPath.row].name ?? "Not available") \(shoppingList[indexPath.row].amount)x"
+        // store indexpath inside cell
+        cell.indexPath = indexPath
         
         return cell
     }
-    
     
 }
