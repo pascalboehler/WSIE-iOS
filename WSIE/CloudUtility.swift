f17a8d0c4d7f683fd73ead2b30f994715c17d4c0
pascalboehler <pascal.boehler@hotmail.de>
2019-06-09 14:25:20 +0200

    Created struct with recipe and created fetchRecipeData

---
+++ WSIE/CloudUtility.swift
@@ -0,0 +1,13 @@
+//
+//  CloudUtility.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 09.06.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import Foundation
+
+func fetchRecipeData() -> [Recipe] {
+    return []
+}
--- WSIE/TabBarViewController.swift
+++ WSIE/TabBarViewController.swift
@@ -16,15 +16,5 @@ class TabBarViewController: UITabBarController {
         // Do any additional setup after loading the view.
         selectedIndex = 2
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
