db4979b8da9e6fae8d325ef54cd4ac03ac0d7de2
pascalboehler <pascal.boehler@hotmail.de>
2019-07-09 16:37:50 +0200

    Added new LoginVC to proj (unable to merge from branch

--- WSIE/LoginViewController.swift
+++ WSIE/LoginViewController.swift
@@ -2,7 +2,7 @@
 //  LoginViewController.swift
 //  WSIE
 //
-//  Created by Pascal Boehler on 26.06.19.
+//  Created by Pascal Boehler on 08.07.19.
 //  Copyright © 2019 Pascal Boehler. All rights reserved.
 //
 
@@ -14,14 +14,30 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    var handle: AuthStateDidChangeListenerHandle?
+    var handle: AuthStateDidChangeListenerHandle!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
-            print("Handler added")
-        })
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if Auth.auth().currentUser != nil {
+            print("quitted vc")
+            print(Auth.auth().currentUser?.uid)
+            performSegue(withIdentifier: "showTabBarViewController", sender: nil)
+            //dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            return
+        })
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -30,39 +46,47 @@ class LoginViewController: UIViewController {
         Auth.auth().removeStateDidChangeListener(handle!)
     }
     
-    @IBAction func loginButtonHandler(_ sender: Any) {
+    @IBAction func LoginButtonHandler(_ sender: Any) {
         print("On login button pressed")
         guard let email = emailTextField.text else {
-            print("Error no email entered")
+            print("Insert an email addr.")
             return
         }
         guard let password = passwordTextField.text else {
-            print("No password entered")
+            print("Please enter your password!")
             return
         }
-        
-        // [START headless_email_auth]
-        Auth.auth().signIn(withEmail: email, password: password) { user, error in
-            if error != nil {
-                print("Something happened \(error)")
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, err) in
+            if let err = err {
+                print("Something went wrong while logging in user with error: \(err)")
             } else {
-                print("Successfully logged in")
-                /*let user = Auth.auth().currentUser
-                print(user!.uid)
-                print(user!.email!)*/
-                self.performSegue(withIdentifier: "showMainApplication", sender: nil)
+                print("Successfully logged in user")
+                self.performSegue(withIdentifier: "showTabBarViewController", sender: nil)
             }
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SignUpButtonHandler(_ sender: Any) {
+        print("On sign up button pressed")
+        guard let email = emailTextField.text else {
+            print("Insert an email addr.")
+            return
+        }
+        guard let password = passwordTextField.text else {
+            print("Please choose a password")
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                //print("Something went wrong while signing up new user with error \(error)")
+                var userExistsAlert = UIAlertController(title: "Error signing up new user", message: error.localizedDescription, preferredStyle: .alert)
+                var okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                userExistsAlert.addAction(okAction)
+                self.present(userExistsAlert, animated: true, completion: nil)
+            } else {
+                print("Successfully created new user")
+                self.performSegue(withIdentifier: "showTabBarViewController", sender: nil)
+            }
+        }
     }
-    */
-
 }
