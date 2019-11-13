1904af50de918c6c5c9169fdfe4bf61256005f81
Pascal Böhler <pascal.boehler@hotmail.de>
2019-11-13 13:33:02 +0100

    Moved Storyboard based app to Old App folder & created new app in App

---
+++ App/WSIE/WSIE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WSIE
+//
+//  Created by Pascal Boehler on 13.11.19.
+//  Copyright © 2019 Pascal Boehler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
